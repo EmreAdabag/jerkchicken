f60b86d14feaa018015e2b76c759f8eea5a0e50e
EmreAdabag <emreadabag@gmail.com>
2023-01-30 11:26:27 -0500

    message mod

--- jerkchickenchecker.py
+++ jerkchickenchecker.py
@@ -15,20 +15,20 @@ menu_url = "https://dining.columbia.edu/cu_dining/rest/menus/nested"
 
 
 def getchickenlist():
-    
+
     chicken_days = []
     today = datetime.today().date
 
     menus_json = requests.get(menu_url).json()
 
     for menu in menus_json:
-    
+
         date_range_fields = menu["date_range_fields"][0]
         date_from = date_range_fields["date_from"]
 
         if datetime.strptime(date_from, '%Y-%m-%dT%H:%M:%S').date == today:
             continue
-            
+
         location = date_range_fields["title"]
 
         if date_range_fields["stations"]:
@@ -36,7 +36,7 @@ def getchickenlist():
                 for meal in station["meals"]:
                     if int(meal) in jerk_chicken_dict:
                         chicken_days.append((location, int(meal)))
-        
+
 
     return list(set(chicken_days))
 
@@ -48,7 +48,7 @@ def tweet(message: str):
 
 
 if __name__ == '__main__':
-    
+
     chickenlist = getchickenlist()
     msg = ''
 
@@ -69,6 +69,6 @@ if __name__ == '__main__':
                 location = "Ferris"
 
             if location in {"JJs", "Chef Mike", "John Jay", "Ferris"}:
-                msg += "{} at {} today{}".format(jerk_chicken_dict[meal], location, "" if i == len(chickenlist)-1 else "\n")
+                msg += "\N{Police Cars Revolving Light} {} at {} today{}".format(jerk_chicken_dict[meal], location, "" if i == len(chickenlist)-1 else "\n")
 
     tweet(msg)
