98b23a1fdfda9baed64d0aa60e2d09829be08d93
Joshua Zhou <joshuazhou35@gmail.com>
2024-02-03 18:31:33 -0500

    Add JerkChickenMeal dataclass

--- models.py
+++ models.py
@@ -9,22 +9,5 @@ class Keyword:
 
 @dataclass(slots=True)
 class JerkChickenMeal:
-    food_name: str
-    is_name: bool
-
-    def __post_init__(self):
-        self.food_name = self.food_name.strip()
-        # self.food_name = self.food_name if is_name else self.food_name + " (contains jerk chicken)"
-
-@dataclass(slots=True)
-class Setting:
-    dining_hall: str
-    meal_of_day: str
-
-    def __post_init__(self):
-        self.meal_of_day = self.meal_of_day.strip().lower()
-
-@dataclass(slots=True)
-class JerkChickenEntry:
-    meals: list[JerkChickenMeal]
-    setting: Setting
+    meal_name: str
+    meal_of_day: str  # Breakfast, Lunch, or Dinner
